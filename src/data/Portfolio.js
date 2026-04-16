56f4f193a3f2647d2e1893bf5738130b95b6eb8d
mehtasumeer3 <mehtasumeer3@gmail.com>
2026-04-17 04:59:52 +0530

    updation

--- src/data/Portfolio.js
+++ src/data/Portfolio.js
@@ -1,44 +1,53 @@
 export const portfolio = {
   name: "Sumeer Mehta",
   titleLine: "MCA Student | Aspiring Full Stack Web Developer",
-  heroQuote: "Building responsive, scalable, and user-friendly web applications with modern technologies",
+  heroQuote:
+    "Building responsive, scalable, and user-friendly web applications with modern technologies",
   socials: {
     github: "https://github.com/mehtasumeer3",
     linkedin: "https://www.linkedin.com/in/sumeer-mehta/",
-    email: "mehtasumeer310@gmail.com"
+    email: "mehtasumeer310@gmail.com",
   },
   about: [
     "Hi, I’m Sumeer Mehta, an aspiring software developer with a passion for creating robust, responsive, and scalable web applications. My journey began during my undergraduate studies, where I started building small web projects to solve everyday problems and quickly fell in love with turning ideas into functional digital solutions.",
-    "With hands-on experience through internships and academic projects, I’ve developed skills across the stack from modern JavaScript frameworks on the frontend to Java and RESTful APIs on the backend. I thrive in collaborative, agile teams and am always eager to learn new tools and best practices to deliver value for users."
+    "With hands-on experience through internships and academic projects, I’ve developed skills across the stack from modern JavaScript frameworks on the frontend to Java and RESTful APIs on the backend. I thrive in collaborative, agile teams and am always eager to learn new tools and best practices to deliver value for users.",
   ],
   skills: [
     {
       title: "Frontend",
-      pills: ["React.js", "JavaScript", "TypeScript", "HTML5", "CSS3", "Bootstrap", "Tailwind CSS"]
+      pills: [
+        "React.js",
+        "JavaScript",
+        "TypeScript",
+        "HTML5",
+        "CSS3",
+        "Bootstrap",
+        "Tailwind CSS",
+      ],
     },
     {
       title: "Backend",
-      pills: ["Java", "REST APIs"]
+      pills: ["Java", "REST APIs"],
     },
     {
       title: "Tools & Practices",
-      pills: ["Git", "GitHub", "VS Code", "Agile & Scrum"]
+      pills: ["Git", "GitHub", "VS Code", "Agile & Scrum","Vercel"],
     },
     {
       title: "Fundamentals",
-      pills: ["DSA", "SQL", "OOP Concepts"]
-    }
+      pills: ["DSA", "SQL", "OOP Concepts"],
+    },
   ],
   experience: [
     {
       company: "Trinity Code Labs",
       role: "Software Development Intern",
-      duration: "Sep 2025 – Dec 2025",
+      duration: "June 2025 – Present",
       bullets: [
         "Developed frontend features to enhance user experience using React and JavaScript.",
         "Participated in debugging sessions and resolved critical bugs in a production environment.",
-        "Collaborated closely with senior developers following agile and Git workflows."
-      ]
+        "Collaborated closely with senior developers following agile and Git workflows.",
+      ],
     },
     {
       company: "Automation Simplified Pvt. Ltd.",
@@ -46,68 +55,99 @@ export const portfolio = {
       duration: "Apr 2024 – Jun 2024",
       bullets: [
         "Worked on full-stack development tasks, including API integration and UI enhancements.",
-        "Strengthened Java and problem-solving skills through real-world project contributions."
-      ]
-    }
+        "Strengthened Java and problem-solving skills through real-world project contributions.",
+      ],
+    },
   ],
   projects: [
     {
+      title: "Financial Dashboard",
+      description:
+        "A finance tracking web app to manage transactions, balances, and spending insights, featuring interactive charts, filtering, and real-time state updates using Zustand.",
+      tech: ["React.js", "JavaScript", "Tailwind CSS", "Zustand"],
+      live: "https://mehtasumeer3.github.io/Financial-Dashboard/",
+      github: "https://github.com/mehtasumeer3/Financial-Dashboard",
+    },
+    {
+      title: "User Management Dashboard",
+      description:
+        "A CRUD-based user dashboard with search, pagination, and API integration, enabling efficient user data management with a responsive UI.",
+      tech: ["React.js", "JavaScript", "Tailwind CSS"],
+      live: "https://mehtasumeer3.github.io/user-dashboard/",
+      github: "https://github.com/mehtasumeer3/user-dashboard",
+    },
+    {
+      title: "AI Decision Engine",
+      description:
+        " A decision-support web app that analyzes user inputs and generates psychology-based suggestions through a clean and structured user interface.",
+      tech: ["React.js", "JavaScript", "Tailwind CSS"],
+      live: "https://mehtasumeer3.github.io/decision-engine/",
+      github: "https://github.com/mehtasumeer3/decision-engine",
+    },
+    {
       title: "Stop Before Zero",
-      description: "A timer-based game web app where users try to stop the countdown as close as possible to zero, built with a responsive UI.",
+      description:
+        "A timer-based game web app where users try to stop the countdown as close as possible to zero, built with a responsive UI.",
       tech: ["React.js", "JavaScript", "CSS3"],
       live: "https://mehtasumeer3.github.io/stop-before-zero",
-      github: "https://github.com/mehtasumeer3/stop-before-zero"
+      github: "https://github.com/mehtasumeer3/stop-before-zero",
     },
     {
       title: "News Monkey",
-      description: "Live news portal fetching and displaying the latest headlines using external news APIs. Features category filtering and a modern UI.",
+      description:
+        "Live news portal fetching and displaying the latest headlines using external news APIs. Features category filtering and a modern UI.",
       tech: ["React.js", "API"],
       live: "",
-      github: "https://github.com/mehtasumeer3/news-monkey"
+      github: "https://github.com/mehtasumeer3/news-monkey",
     },
     {
       title: "Drag & Drop Notes",
-      description: "Sticky notes web application allowing users to create, edit, and drag notes using intuitive drag-and-drop interactions.",
+      description:
+        "Sticky notes web application allowing users to create, edit, and drag notes using intuitive drag-and-drop interactions.",
       tech: ["React.js", "Tailwind CSS", "TypeScript"],
       live: "https://mehtasumeer3.github.io/drag-drop-sticky-notes/",
-      github: "https://github.com/mehtasumeer3/drag-drop-sticky-notes"
+      github: "https://github.com/mehtasumeer3/drag-drop-sticky-notes",
     },
     {
       title: "Investment Calculator",
-      description: "Calculates compound interest and provides year-wise investment insights with dynamic charts and data visualization.",
+      description:
+        "Calculates compound interest and provides year-wise investment insights with dynamic charts and data visualization.",
       tech: ["React.js", "JavaScript", "CSS3"],
       live: "https://mehtasumeer3.github.io/investment-calculator/",
-      github: "https://github.com/mehtasumeer3/investment-calculator"
+      github: "https://github.com/mehtasumeer3/investment-calculator",
     },
     {
       title: "Think Pair",
-      description: "Implemented a Think-Pair game with dynamic grid generation (2×2 to 15×15), state management for flipped cards, match detection, and scalable gameplay.",
+      description:
+        "Implemented a Think-Pair game with dynamic grid generation (2×2 to 15×15), state management for flipped cards, match detection, and scalable gameplay.",
       tech: ["React.js", "JavaScript", "CSS3"],
       live: "https://mehtasumeer3.github.io/Memory-Game/",
-      github: "https://github.com/mehtasumeer3/Memory-Game"
+      github: "https://github.com/mehtasumeer3/Memory-Game",
     },
     {
       title: "Zero-kata",
-      description: "A classic Tic-Tac-Toe game with an interactive UI, allowing two players to play turn-by-turn. Includes win, draw detection and smooth game reset functionality.",
+      description:
+        "A classic Tic-Tac-Toe game with an interactive UI, allowing two players to play turn-by-turn. Includes win, draw detection and smooth game reset functionality.",
       tech: ["React.js", "JavaScript", "CSS3"],
       live: "https://mehtasumeer3.github.io/zero-kata/",
-      github: "https://github.com/mehtasumeer3/zero-kata"
+      github: "https://github.com/mehtasumeer3/zero-kata",
     },
     {
       title: "Todo App",
-      description: "Built a Todo web app that allows users to add, delete, and manage daily tasks efficiently with a clean and intuitive interface.",
+      description:
+        "Built a Todo web app that allows users to add, delete, and manage daily tasks efficiently with a clean and intuitive interface.",
       tech: ["React.js", "JavaScript", "CSS3"],
       live: "https://mehtasumeer3.github.io/todo-app/",
-      github: "https://github.com/mehtasumeer3/todo-app"
+      github: "https://github.com/mehtasumeer3/todo-app",
     },
     {
       title: "Text Utils",
-      description: "Developed a Text Utility application for text analysis and transformation, featuring case conversion, text cleanup, copy functionality, and real-time text statistics.",
+      description:
+        "Developed a Text Utility application for text analysis and transformation, featuring case conversion, text cleanup, copy functionality, and real-time text statistics.",
       tech: ["React.js", "JavaScript", "CSS3"],
       live: "https://mehtasumeer3.github.io/Text-Utils/",
-      github: "https://github.com/mehtasumeer3/Text-Utils"
+      github: "https://github.com/mehtasumeer3/Text-Utils",
     },
-    
   ],
   education: [
     {
@@ -115,21 +155,22 @@ export const portfolio = {
       subtitle: "Bharati Vidyapeeth IMED, Pune, India",
       meta: {
         cgpa: "8/10",
-        duration: "2024 – 2026"
-      }
+        duration: "2024 – 2026",
+      },
     },
     {
       title: "Bachelor of Computer Applications (BCA)",
-      subtitle: "Bharati Vidyapeeth Institute of Management and Research, Delhi, India",
+      subtitle:
+        "Bharati Vidyapeeth Institute of Management and Research, Delhi, India",
       meta: {
         cgpa: "9/10",
-        duration: "2021 – 2024"
-      }
-    }
+        duration: "2021 – 2024",
+      },
+    },
   ],
   contact: {
     email: "mehtasumeer310@gmail.com",
     phone: "9773816733",
-    location: "Delhi, India"
-  }
+    location: "Delhi, India",
+  },
 };
